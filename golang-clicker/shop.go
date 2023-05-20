5c4fa006f4394f7ec75c6ee9ebb4695fbbd066d6
Contengent <103345406+Contengent@users.noreply.github.com>
2023-05-20 19:44:07 +0000

    Update shop.go

--- golang-clicker/shop.go
+++ golang-clicker/shop.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"math"
 	_ "math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type shopItem struct {
-	itemName         string
+	name             string
 	currentlyOwned   float64
 	currentPrice     float64
 	priceMultiplier  float64
@@ -19,16 +20,21 @@ func (item *shopItem) purchaseItem(iter int, pM int) {
 	for iter < pM && ebiten.IsKeyPressed(item.upgradeKey) {
 		if number >= item.currentPrice {
 			number -= item.currentPrice
-			item.currentPrice *= item.priceMultiplier
 			item.currentlyOwned += item.upgradeIncrement
-			iter++
+
+			if item.name == "cpsToThePower" {
+				item.currentPrice = math.Pow(item.currentPrice, item.priceMultiplier)
+			} else {
+				item.currentPrice *= item.priceMultiplier
+			}
 		}
+		iter++
 	}
 }
 
 func (item shopItem) itemInformation() string {
 
-	info := "\n" + item.itemName + "($" + numberFormatting(item.currentPrice, 0) + "): " + numberFormatting(item.currentlyOwned, 2)
+	info := "\n" + item.name + "($" + numberFormatting(item.currentPrice, 0) + "): " + numberFormatting(item.currentlyOwned, 2)
 
 	return info
 
