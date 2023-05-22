fc4e6da4ffb2f2d5005d8f600a14e3c0bb2037b3
Contengent <103345406+Contengent@users.noreply.github.com>
2023-05-22 21:40:59 +0000

    Update main.go

--- golang-clicker/main.go
+++ golang-clicker/main.go
@@ -157,7 +157,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Golang clicker game")
+	ebiten.SetWindowTitle("Go-clicker")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
